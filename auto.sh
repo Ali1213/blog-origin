729ed350429fc56572ca3b06e12c891e3b91f1cd
Ali1213 <yusili2000@163.com>
2017-09-08 15:33:09 +0800

    auto commit by ali at

--- auto.sh
+++ auto.sh
@@ -2,7 +2,7 @@ hexo generate
 git add .
 git commit -a -m "auto commit by ali at $DATE"
 git push origin master
-mv -bf public/* ../Ali1213.github.io
+mv -f public/* ../Ali1213.github.io
 cd  ../Ali1213.github.io
 git add .
 git commit -a -m "auto commit by ali at $DATE
