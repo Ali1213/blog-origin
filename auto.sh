5206283a726f77063ea6e01bb3ec76427ca54b66
Ali1213 <yusili2000@163.com>
2017-09-08 15:44:57 +0800

    auto commit by ali at 20170908

--- auto.sh
+++ auto.sh
@@ -1,9 +1,88 @@
 hexo generate
+
+if [ $? ne 0 ]
+then
+    echo "failed hexo generate"
+    echo $?
+    exit
+fi
+
 git add .
-git commit -a -m "auto commit by ali at $DATE"
+
+if [ $? ne 0 ]
+then
+    echo "failed to git add ."
+    echo $?
+    exit
+fi
+
+git commit -a -m "auto commit by ali at `date +%Y%m%d`"
+
+if [ $? ne 0 ]
+then
+    echo "failed to git commit "
+    echo $?
+    exit
+fi
+
 git push origin master
+
+
+if [ $? ne 0 ]
+then
+    echo "failed to git push "
+    echo $?
+    exit
+fi
+
 mv -f public/* ../Ali1213.github.io
+
+
+if [ $? ne 0 ]
+then
+    echo "failed to mv "
+    echo $?
+    exit
+fi
+
+
 cd  ../Ali1213.github.io
+
+
+if [ $? ne 0 ]
+then
+    echo "failed to cd  "
+    echo $?
+    exit
+fi
+
+
 git add .
-git commit -a -m "auto commit by ali at $DATE"
+
+
+if [ $? ne 0 ]
+then
+    echo "failed to git add "
+    echo $?
+    exit
+fi
+
+git commit -a -m "auto commit by ali at `date +%Y%m%d`"
+
+
+if [ $? ne 0 ]
+then
+    echo "failed to git commit "
+    echo $?
+    exit
+fi
+
 git push origin master
+
+if [ $? ne 0 ]
+then
+    echo "failed to 
+git push  "
+    echo $?
+    exit
+fi
