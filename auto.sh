6ed7eb1c07c94beb633dfcdd7c32b1222fff643e
Ali1213 <yusili2000@163.com>
2017-09-08 15:33:45 +0800

    auto commit by ali at

--- auto.sh
+++ auto.sh
@@ -5,5 +5,5 @@ git push origin master
 mv -f public/* ../Ali1213.github.io
 cd  ../Ali1213.github.io
 git add .
-git commit -a -m "auto commit by ali at $DATE
+git commit -a -m "auto commit by ali at $DATE"
 git push origin master
