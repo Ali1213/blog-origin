601b3b065af66501443ed6e5fde892bc550ab099
Ali1213 <yusili2000@163.com>
2017-09-08 15:52:24 +0800

    auto commit by ali at 20170908

--- auto.sh
+++ auto.sh
@@ -1,88 +1,19 @@
-hexo generate
+#!/bin/bash
 
-if [ $? -ne 0 ]
-then
-    echo "failed hexo generate"
-    echo $?
-    exit
-fi
+hexo generate &&    /
 
-git add .
+git add . && /
 
-if [ $? -ne 0 ]
-then
-    echo "failed to git add ."
-    echo $?
-    exit
-fi
+git commit -a -m "auto commit by ali at `date +%Y%m%d`" && /
 
-git commit -a -m "auto commit by ali at `date +%Y%m%d`"
+git push origin master && /
 
-if [ $? -ne 0 ]
-then
-    echo "failed to git commit "
-    echo $?
-    exit
-fi
+mv -f public/* ../Ali1213.github.io && /
 
-git push origin master
+cd  ../Ali1213.github.io && /
 
+git add . && /
 
-if [ $? -ne 0 ]
-then
-    echo "failed to git push "
-    echo $?
-    exit
-fi
+git commit -a -m "auto commit by ali at `date +%Y%m%d`" && /
 
-mv -f public/* ../Ali1213.github.io
-
-
-if [ $? -ne 0 ]
-then
-    echo "failed to mv "
-    echo $?
-    exit
-fi
-
-
-cd  ../Ali1213.github.io
-
-
-if [ $? -ne 0 ]
-then
-    echo "failed to cd  "
-    echo $?
-    exit
-fi
-
-
-git add .
-
-
-if [ $? -ne 0 ]
-then
-    echo "failed to git add "
-    echo $?
-    exit
-fi
-
-git commit -a -m "auto commit by ali at `date +%Y%m%d`"
-
-
-if [ $? -ne 0 ]
-then
-    echo "failed to git commit "
-    echo $?
-    exit
-fi
-
-git push origin master
-
-if [ $? -ne 0 ]
-then
-    echo "failed to 
-git push  "
-    echo $?
-    exit
-fi
+git push origin master && /
